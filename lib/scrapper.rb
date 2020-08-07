eb39baafd3278fb5610f1dfb478e95cf24679be2
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-08 01:24:01 +0300

    add next page feature

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,10 +1,15 @@
 class Scrapper
-    def initialize site
-        @site = site
-    end
+  def initialize(site)
+    @site = site
+    @page = 1 
+  end
 
-    def scrap
-        @doc = Nokogiri::HTML(URI.open(site.base_url))
-    end
+  def scrap
+    Nokogiri::HTML(URI.open("#{@site.base_url}/?action=search&page=#{@page}&view=list"))
+  end
 
+  def next_page
+    @page += 1
+    scrap
+  end
 end
