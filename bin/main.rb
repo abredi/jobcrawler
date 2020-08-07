685493a1b8f287d1d2e2ff8a0716fc7ede8dcb38
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-08 02:29:53 +0300

    add user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,37 @@
 #! /usr/bin/env ruby
 require_relative '../lib/ethiojobs.rb'
 
-ej = EthioJobs.new
-# puts ej.current_page
+def print(ejobs)
+  ejobs.current_page.each do |job|
+    puts '***************************************************'
+    puts job[:title]
+    puts job[:link]
+    puts job[:company]
+    puts job[:company_link]
+    puts job[:work_place]
+    puts job[:level]
+    puts job[:date]
+    puts job[:detail]
+    puts '***************************************************'
+  end
+  prompt(ejobs)
+end
 
-puts ej.tt
+def next_step(inp, ejobs)
+  case inp
+  when '#'
+    ejobs.next_page
+    print(ejobs)
+  else
+    exit(1)
+  end
+end
+
+def prompt(ejobs)
+  puts 'If you want to see next page enter 1 \n if you want to exit enter any key'
+  inp = gets.chomp
+  next_step(inp, ejobs)
+end
+
+ejobs = EthioJobs.new
+print(ejobs)
--- lib/ethiojobs.rb
+++ lib/ethiojobs.rb
@@ -6,7 +6,7 @@ class EthioJobs < Scrapper
   attr_accessor :base_url
   attr_reader :current_page
   def initialize
-    @base_url = 'http://www.ethiojobsXXXXX.net/search-results-jobs'
+    @base_url = 'http://www.ethiojobs.net/search-results-jobs'
     super(self)
     snatch_data
   end
@@ -16,19 +16,17 @@ class EthioJobs < Scrapper
     snatch_data
   end
 
-  private
-
   def snatch_data
     @current_page = []
     scrap.css('table .listing-section').each do |link|
       @current_page.push(
         {
           title: link.css('h2 a').text.strip,
-          link: link.css('h2 a').text.strip,
+          link: link.css('h2 a').first['href'].strip,
           company: link.css('.company-name').text.strip,
-          company_link: link.css('.company-name').text.strip,
+          company_link: link.css('.company-name').first['href'].strip,
           work_place: link.css('.work-palce').text.strip,
-          level: link.css('.pull-left > span:nth-child(4)').text.strip,
+          level: link.css('.pull-left > span:nth-child(7)').text.strip,
           date: link.css('.pull-left > span:nth-child(9)').text.strip,
           detail: link.css('.show-brief').text.strip
         }
